e0bede5fe786050fd052127b303d2281959eff27
KodjoCode <appohfaiths@gmail.com>
2024-05-10 17:06:38 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useBoardStore } from '~/stores/boardStore';
+
+const boardStore = useBoardStore();
+</script>
+
+<template>
+  <main>
+    <h1>Build a trello clone home page</h1>
+    <pre>{{ boardStore.board }}</pre>
+  </main>
+</template>
+
+<style scoped></style>
